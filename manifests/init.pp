cba3186d9b853aaa8b207bcc1b17e8f02f9ce53d
Daniel werdermann <dwerdermann@web.de>
2014-07-04 00:53:29 +0200

    add code docu

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,56 @@
 # == Class: grafana
 #
-# This class downloads and installs grafana.
-# SHOULD NOT be called directly.
+# This class installs and configures grafana.
 #
 # === Parameters
 #
-# None.
+# [*version*]
+#   Version of grafan to be installed.
+#   Default is '1.6.1'
+# [*install_dir*]
+#   Install directory of grafana.
+#   Default is '/opt'
+# [*graphite_host*]
+#   Hostname of graphite server.
+#   Default is 'localhost'
+# [*graphite_port*]
+#   Port of graphite service.
+#   Default is 80
+# [*elasticsearch_host*]
+#   Hostname of elasticsearch. You will need an elasticsearch
+#   for saving dashboards
+#   Default is '' (empty)
+# [*elasticsearch_por*]
+#   Port of elasticsearch service.
+#   Default is 9200
+# [*influxdb_host*]
+#   Hostname of influxdb.
+#   Default is '' (empty)
+# [*influxdb_port*]
+#   Port of influxdb.
+#   Default is 8086
+# [*influxdb_dbpath*]
+#   DB path of influxdb.
+#   Default is '/db/grafana'
+# [*influxdb_user*]
+#   Name of db user.
+#   Default is 'grafana'
+# [*influxdb_pass*]
+#   Password of db user.
+#   Default is 'grafana'
+# [*timezone_offset*]
+#   If you experiance problems with zoom, it is probably caused by timezone diff between
+#   your browser and the graphite-web application. timezoneOffset setting can be used to have Grafana
+#   translate absolute time ranges to the graphite-web timezone.
+#   Example:
+#     If TIME_ZONE in graphite-web config file local_settings.py is set to America/New_York, then set
+#     timezoneOffset to "-0500" (for UTC - 5 hours)
+#   Example:
+#     If TIME_ZONE is set to UTC, set this to "0000"
+#   Default is '0000'
+# [*playlist_timespan*]
+#   Playlist timespan.
+#   Default is '1m'
 #
 class grafana (
   $version            = '1.6.1',
@@ -17,8 +62,8 @@ class grafana (
   $influxdb_host      = '',
   $influxdb_port      = 8086,
   $influxdb_dbpath    = '/db/grafana',
-  $influxdb_user      = 'admin',
-  $influxdb_pass      = 'admin',
+  $influxdb_user      = 'grafana',
+  $influxdb_pass      = 'grafana',
   $timezone_offset    = '0000',
   $playlist_timespan  = '1m'
 ) {
